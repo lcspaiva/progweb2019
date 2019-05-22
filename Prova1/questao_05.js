b7f9126eb14fea46096e2da30ffd1e9ae5417aaf
Carlos William <cwtm@icomp.ufam.edu.br>
2019-05-22 08:42:04 -0400

    submetendo a questao5

---
+++ Prova1/questao_05.js
@@ -0,0 +1,35 @@
+class Empregado{
+    constructor(nome, salario){
+        this.nome = nome
+        if (salario < 0.0){
+            this.salario = 0.0
+        }else{
+            this.salario = salario
+        }
+    }
+    getSalario(){return this.salario}
+    setSalario(salario){
+        if (salario < 0.0){
+            this.salario = 0.0
+        }else{
+            this.salario = salario
+        }
+    }
+
+    getNome(){return this.nome}
+    setNome(nome){this.nome=nome}
+}
+
+emp1 = new Empregado("Lucas", 8000)
+emp2 = new Empregado("David", 20000)
+
+console.log("Salario base")
+console.log(emp1.getSalario())
+console.log(emp2.getSalario())
+
+emp1.setSalario(emp1.getSalario() + emp1.getSalario() * 0.1)
+emp2.setSalario(emp2.getSalario()+ emp2.getSalario() * 0.1)
+
+console.log("Depois do aumento")
+console.log(emp1.getSalario())
+console.log(emp2.getSalario())
